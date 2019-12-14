1e6134a16bf92e211a310080a30158869c15c287
Silver Edge <2628056-damiendark@users.noreply.gitlab.com>
2019-12-15 00:49:34 +0500

    react-build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = require('paths')
+const path = require('path')
 // Route Imports
 const users = require("./routes/api/users");
 const conversations = require('./routes/api/conversations');
